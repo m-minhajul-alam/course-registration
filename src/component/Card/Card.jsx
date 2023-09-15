9d1d7540c2e83b071245591130e04f1f968cff9e
Minhajul-alam <alam.minhajul@outlook.com>
2023-09-15 22:22:41 +0600

    Display data as cards in UI

--- src/component/Card/Card.jsx
+++ src/component/Card/Card.jsx
@@ -8,9 +8,11 @@ const Card = ({ card }) => {
         <div>
 
             <div className="bg-white p-5 rounded-xl h-full">
+
                 <div className="flex justify-center items-center">
                     <img className="rounded-lg " src={cover_img} alt="" />
                 </div>
+
                 <div className="my-3">
                     <h4 className="text-md font-semibold my-3">{title}</h4>
                     <p className="text-xs font-normal text-gray-400 my-3">{description}</p>
@@ -25,9 +27,11 @@ const Card = ({ card }) => {
                         </div>
                     </div>
                 </div>
+
                 <div className='mt-auto'>
                     <button className="text-sm text-white font-semibold py-2 w-full rounded-lg bg-blue-500 mt-auto">Select</button>
                 </div>
+                
             </div>
 
         </div>
