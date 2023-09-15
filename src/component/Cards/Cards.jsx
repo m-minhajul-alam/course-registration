2282929c4ffe5f56c62e29783bbcb1554d5196c5
Minhajul-alam <alam.minhajul@outlook.com>
2023-09-15 19:01:37 +0600

    create fake data and load data

--- src/component/Cards/Cards.jsx
+++ src/component/Cards/Cards.jsx
@@ -1,4 +1,14 @@
+import { useEffect, useState } from "react";
+
 const Cards = () => {
+
+    const [cards, setCards] = useState()
+
+    useEffect(() => {
+        fetch('./data.json')
+            .then(res => res.json())
+            .then(data => setCards(data));
+    }, [])
 
     return (
         <div>
